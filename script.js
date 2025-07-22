35159b15dfd31e870dfe7b25f170d06e0c859c98
unknown <titarmareyash6@gmail.com>
2025-07-22 19:10:27 +0530

    intianl commit

---
+++ script.js
@@ -0,0 +1,13 @@
+/*------------ type-js-----------*/
+
+var typed = new Typed('#auto-type1', {
+    strings: ['Your Gateway to Tech Excellence !','a Community of Tech Enthusiasts!','Dedicated to Your Coding Dreams!'],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true
+    });
+
+
+
+
+
